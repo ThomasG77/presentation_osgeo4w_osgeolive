9c5e0e78131fce02d8ef5408f0743b9019d20df8
Thomas <thomas_gratier@yahoo.fr>
2014-05-22 23:03:58 +0200

    MAJ document + renommage accueil en index lors de la génération du document

--- generate_slide_from_pandoc.sh
+++ generate_slide_from_pandoc.sh
@@ -1,1 +1,1 @@
-pandoc -t revealjs --slide-level=2 --template=osgeo.revealjs --css=reveal.js/lib/css/zenburn.css -s presentation_osgeo4w_osgeolive.txt -o presentation_osgeo4w_osgeolive.html
+pandoc -t revealjs --slide-level=2 --template=osgeo.revealjs --css=reveal.js/lib/css/zenburn.css -s presentation_osgeo4w_osgeolive.txt -o index.html
